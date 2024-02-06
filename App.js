5a5ac2c52e7e20eafa811282639914a371103053
realsingeriu <sda1613@gmail.com>
2024-02-06 09:26:17 +0900

    커스텀 폰트 설치 및 설정

--- App.js
+++ App.js
@@ -1,10 +1,34 @@
 import { StatusBar } from "expo-status-bar";
+import { useCallback } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    "Pretend-Regular": require("./assets/fonts/Pretendard-Regular.otf"),
+    "Pretend-Medium": require("./assets/fonts/Pretendard-Medium.otf"),
+    "Pretend-Bold": require("./assets/fonts/Pretendard-Bold.otf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>헬로우 월드!</Text>
+    <View style={styles.container} onLayout={onLayoutRootView}>
+      <Text style={{ fontSize: 30 }}>프리텐디드 폰트</Text>
+      <Text style={{ fontFamily: "Pretend-Regular", fontSize: 30 }}>
+        세상에 이런 폰트가!
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
